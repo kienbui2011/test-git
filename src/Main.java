7eb246e0d64a2485d2535d2e162a342a70b10d4a
kienbui2011 <kien.bui.20112002@gmail.com>
2023-03-11 16:52:06 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap 1 so ban muon bat ky: ");
+        int ns = sc.nextInt();
+        System.out.println("so ban nhap tren ban phim la: "+ns);
+
+    }
+}
